37b04f620ab6aaab062b508366418b2c0ad902f6
Alina <alina.stolbikova.com>
2024-01-22 18:04:51 +0100

    added pretiter, styling, geolocation

---
+++ App.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const View = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.Text`
+  font-size: 20px;
+  color: red;
+`;
--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import * as S from './App.styles';
+
+async function getLocation() {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    console.error('Permission to access location was denied');
+    return;
+  }
+
+  let location = await Location.getCurrentPositionAsync({});
+  console.log(location);
+}
 
 export default function App() {
+  getLocation();
   return (
     <View style={styles.container}>
-      <Text>WOOOOOOW!</Text>
-      <Text>LALALA</Text>
+      <S.Text>This week</S.Text>
+      <S.Text>Explore</S.Text>
       <StatusBar style="auto" />
     </View>
   );
